3297a3dc4814944da6769f3c9ccd4b5a71762298
Marc Naples <Marc Naples>
2016-10-24 20:22:41 -0400

    first commit

---
+++ findmean.R
@@ -0,0 +1,5 @@
+findmean <- function(meanvector){
+  vectormean <- mean(meanvector)
+  vectormean}
+vec <- c(1,2,3,4,5)
+findmean(vec)
---
+++ runfunction.R
@@ -0,0 +1,5 @@
+runfunction <- function(x){
+  return(findmean(x))
+}
+vec <- c(1,2,3,4,5)
+runfunction(vec)
